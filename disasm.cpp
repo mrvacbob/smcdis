9c2d4a462d60803695c36497c455dcd15bc89f5a
Alexander Strange <astrange@ithinksw.com>
2009-12-03 16:35:35 -0500

    Remove unused macros

--- disasm.cpp
+++ disasm.cpp
@@ -136,10 +136,6 @@ static void update_state(instruction &i, cpu_state &s)
 	s.pc += i.size;
 }
 
-#define advance_pc(x) s.pc += x;
-#define x_16() (!s.emu && ((s.status & 0x10) == 0))
-#define a_16() (!s.emu && ((s.status & 0x20) == 0))
-
 static const char *reg(instruction_def &i)
 {
 	switch(i.regs)
